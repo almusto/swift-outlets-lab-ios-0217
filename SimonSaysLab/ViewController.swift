4712004e2783a2f5edda0be52f71cdf048af7feb
almusto <ales.musto@gmail.com>
2017-01-30 13:35:21 -0500

    indeed

--- SimonSaysLab/ViewController.swift
+++ SimonSaysLab/ViewController.swift
@@ -16,9 +16,46 @@ class ViewController: UIViewController {
     var simonSaysGame = SimonSays()
     var buttonsClicked = 0
     
+  @IBAction func redPressed(_ sender: Any) {
+    simonSaysGame.guessRed()
+    buttonsClicked += 1
+    winner()
+  }
+  @IBAction func greenPressed(_ sender: Any) {
+    simonSaysGame.guessGreen()
+    buttonsClicked += 1
+    winner()
+  }
+  @IBAction func yellowPressed(_ sender: Any) {
+    simonSaysGame.guessYellow()
+    buttonsClicked += 1
+    winner()
+  }
+  @IBAction func bluePressed(_ sender: Any) {
+    simonSaysGame.guessBlue()
+    buttonsClicked += 1
+    winner()
+  }
     override func viewDidLoad() {
         super.viewDidLoad()
+      winLabel.isHidden = true
     }
+  func winner() {
+    if (buttonsClicked == 5) {
+        if (simonSaysGame.wonGame() == true) {
+          winLabel.text = "you won!"
+          winLabel.isHidden = false
+          startGameButton.isHidden = false
+          buttonsClicked = 0
+        } else {
+          winLabel.text = "nope, try again"
+          winLabel.isHidden = false
+          startGameButton.isHidden = false
+          buttonsClicked = 0
+        }
+
+    }
+  }
 }
 
 // MARK: - SimonSays Game Methods
@@ -27,6 +64,7 @@ extension ViewController {
     @IBAction func startGameTapped(_ sender: UIButton) {
         UIView.transition(with: startGameButton, duration: 0.9, options: .transitionFlipFromBottom , animations: {
             self.startGameButton.isHidden = true
+            self.winLabel.isHidden = true
             }, completion: nil)
         
         displayTheColors()
